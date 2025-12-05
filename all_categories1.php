d8735db1d006ef40d87328a054b2df8b8c8b2187
Ritik Rosan Sahoo <ritiksahoors@gmail.com>
2025-12-05 18:07:15 +0530

    changes

---
+++ all_categories1.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description"
+        content="ShopNow offers the latest fashion and accessories for women, men, and kids. Discover exclusive deals, new arrivals, and a seamless online shopping experience" />
+    <meta name="keywords"
+        content="online shopping, e-commerce, women's fashion, men's clothing, kids apparel, ShopNow, latest deals, new arrivals" />
+    <meta name="author" content="origin-innovation" />
+    <title>ShopNow - Online Shopping for Women, Men & Kids</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="assets/css/main.css?v=3.8">
+
+</head>
+
+<body>
+
+    <!-- <div class="utkalika-loader">
+        <div class="utkalika-title">ଉତ୍କଳିକା </div>
+        <div class="utkalika-subtitle">Utkalikart</div>
+        <div class="utkalika-loading-text">Loading<span>.</span><span>.</span><span>.</span></div>
+    </div> -->
+
+    <?php include 'common/header.php' ?>
+
+    <button id="ar-goTopBtn" class="ar-go-top-btn" aria-label="Go to top">
+        <i class="bi bi-chevron-up"></i>
+    </button>
+
+
+    <section>
+        <div class="">
+            <!-- HERO BANNER -->
+            <div id="mainSlider" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/DECEMBER/4/2CeinmPS_f36801704f2a46e1ab71a098c0379d14.jpg" class="d-block w-100 img-fluid" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/DECEMBER/4/2CeinmPS_f36801704f2a46e1ab71a098c0379d14.jpg" class="d-block w-100 img-fluid" alt="">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/DECEMBER/4/2CeinmPS_f36801704f2a46e1ab71a098c0379d14.jpg" class="d-block w-100 img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- FLAT 500 OFFER -->
+    <div class="container-fluid mt-3">
+        <img src="assets/img/Screenshot 2025-12-05 173221.png" class="img-fluid w-100" alt="">
+    </div>
+
+    <!-- BANK OFFERS -->
+    <div class="container-fluid mt-4">
+        <img src="assets/img/Screenshot 2025-12-05 173327.png" class="img-fluid w-100" alt="">
+    </div>
+
+    <!-- OPENING OFFERS -->
+    <div class="container-fluid my-4">
+        <img src="assets/img/Screenshot 2025-12-05 173427.png" class="img-fluid w-100" alt="">
+    </div>
+
+    <!-- TRENDING CATEGORIES -->
+    <section class="container my-5">
+        <h3 class="fw-bold mb-3">Trending on Myntra</h3>
+        <div class="row g-3">
+            <div class="col-6 col-md-3 col-lg-2">
+                <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid border p-4 rounded">
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid border p-4 rounded">
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid border p-4 rounded">
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid border p-4 rounded">
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid border p-4 rounded">
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid border p-4 rounded">
+            </div>
+        </div>
+    </section>
+
+    <!-- SHOP BY CATEGORY -->
+    <section class="container my-5">
+        <h3 class="fw-bold mb-3">SHOP BY CATEGORY</h3>
+        <div class="row g-4">
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-2">
+                <div class="category-card">
+                    <img src="//neemans.com/cdn/shop/files/Slides_9799e993-257e-4334-a853-ff938ac7bcb9.png?v=1759889764&width=300" class="img-fluid rounded p-4 border">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- APP BANNER -->
+    <div class="container-fluid my-5">
+        <img src="assets/img/banners/app-banner.jpg" class="img-fluid w-100">
+    </div>
+
+
+
+    <?php include 'common/footer.php' ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.10.2/lottie.min.js"></script>
+    <script src="assets/js/main.js?v=1.3"></script>
+</body>
+
+</html>
