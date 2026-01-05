202d87aec0c6c5d5a3e5dab4420c55d2b265d4b0
Ritik Rosan Sahoo <ritiksahoors@gmail.com>
2026-01-05 18:09:24 +0530

    changes

--- offers.php
+++ offers.php
@@ -10,7 +10,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/main.css?v=4.1">
+    <link rel="stylesheet" href="assets/css/main.css?v=4.2">
 </head>
 
 <body id="offers-page">
@@ -264,11 +264,11 @@
 
     <?php include 'common/footer.php' ?>
 
+    <script src="assets/js/main.js?v=1.5"></script>
+    <script src="assets/js/offers.js?v=1.6"></script>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/dist/confetti.browser.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.10.2/lottie.min.js"></script>
-    <script src="assets/js/main.js?v=1.4"></script>
-    <script src="assets/js/offers.js?v=1.5"></script>
 </body>
 
 </html>
