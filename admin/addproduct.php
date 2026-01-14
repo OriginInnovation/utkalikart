eface64a88dced53f6acddbdfe63deb31f14b1cb
monalisadas2000 <143864885+monalisadas2000@users.noreply.github.com>
2026-01-14 19:09:05 +0530

    updated

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -516,5 +516,8 @@ if ($userid === NULL) {
 <?php include 'common/footer.php'; ?>
 <!-- for catalogue dropdown -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<script>
+
+</script>
 
 </html>
